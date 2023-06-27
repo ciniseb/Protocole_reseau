a1941477a415b9d3332dbac90cd840b1ada63d04
Boie0601 <Boie0601@usherbrooke.ca>
2023-06-26 23:03:08 -0400

    :)

--- src/classesAuxiliaires/CoucheLiaison.java
+++ src/classesAuxiliaires/CoucheLiaison.java
@@ -1,26 +1,88 @@
 package classesAuxiliaires;
 
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+
+
 public class CoucheLiaison extends Couche
 {
-
+    int erreurCRC = 0;
+    int paquetTransmit = 0;
+    int paquetRecut = 0;
+    int paquetPerdu =0;
     @Override
     public String traite(Requete donnees)
     {
+
         if(prochaine_couche instanceof CouchePhysique)
         {
-            //TODO: Traitement de la couche actuelle
+            donnees.setBytes(calculerCRC(donnees.getBytes()));
+
+            paquetTransmit ++;
 
             prochaine_couche.traite(donnees);
             return "Couche Liaison --> Couche Physique\n" + prochaine_couche.traite(donnees);
         }
-        else if(prochaine_couche instanceof CoucheTransport)
-        {
-            //TODO: Traitement de la couche actuelle
+        else if(prochaine_couche instanceof CoucheTransport) {
 
-            prochaine_couche.traite(donnees);
-            return "Couche Liaison --> Couche Transport\n" + prochaine_couche.traite(donnees);
+            if(verifierCRC(donnees.getBytes(), POLYNOMIAL))
+            {
+                erreurCRC++;
+            }
+
+            if(donnees.getString() != null)
+            {
+                paquetRecut++;
+            }
+
+                paquetPerdu = paquetTransmit- paquetRecut;
+
+                prochaine_couche.traite(donnees);
+                return "Couche Liaison --> Couche Transport\n" + prochaine_couche.traite(donnees);
         }
 
         return "Couche Liaison : traitement impossible";
     }
+
+
+    private static final int POLYNOMIAL = 0xEDB88320;
+
+    public static byte[] calculerCRC(byte[] data) {
+        int crc = 0xFFFFFFFF;
+        for (byte b : data) {
+            crc ^= b;
+            for (int i = 0; i < 8; i++) {
+                if ((crc & 1) == 1) {
+                    crc = (crc >>> 1) ^ POLYNOMIAL;
+                } else {
+                    crc >>>= 1;
+                }
+            }
+        }
+        crc ^= 0xFFFFFFFF;
+        byte[] crcBytes = new byte[4];
+        crcBytes[0] = (byte) (crc & 0xFF);
+        crcBytes[1] = (byte) ((crc >>> 8) & 0xFF);
+        crcBytes[2] = (byte) ((crc >>> 16) & 0xFF);
+        crcBytes[3] = (byte) ((crc >>> 24) & 0xFF);
+        return crcBytes;
+    }
+
+    public static boolean verifierCRC(byte[] data, long crc) {
+        crc ^= 0xFFFFFFFF;
+        for (byte b : data) {
+            crc ^= b;
+            for (int i = 0; i < 8; i++) {
+                if ((crc & 1) == 1) {
+                    crc = (crc >>> 1) ^ POLYNOMIAL;
+                } else {
+                    crc >>>= 1;
+                }
+            }
+        }
+        return crc == 0;
+    }
+
 }
+
+
